5c6b63e425d72271af4ef9c8446a151516cef3fa
yabuki829 <syodai829@icloud.com>
2024-05-22 20:26:21 +0900

    fix

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Idea(models.Model):
+    title = models.CharField(max_length=255),
+    desctiption = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
--- api/views.py
+++ api/views.py
@@ -9,27 +9,32 @@ import json
 
 def index(request):
     client = OpenAI(api_key=settings.API_KEY)   
+
+
+    category = "" # 事業,サービス、
+    idea = ""   # ユーザーに入力
+    target = "" # ユーザーに入力
     completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-                {"role": "system", "content": "あなたは一流のWebサービスの企画担当で、独創的で、まだ誰も思いついていないような、新しいサービスのアイデア考えることができます。"},
-                {"role": "user", "content": 
-                """
-               アイデアのタイトルと説明を水平思考で考え2つ出してください。
-                # カテゴリ
-                Webサービス
-
-                # 情報
-                ・ターゲット：アイデアを考えている人
-                ・満たしたいニーズ：アイデアを考えるのを手助けしたい
-
-                以下のように出力してください
-                サービスのタイトル(title)そのサービスの詳しい説明やアイデア(explain)をjson形式で出力してください
-                "results":[{title:"",explain:""},{title:"",explain:""}]
-
-                """}
-                
+        {"role": "system", "content": "あなたは一流のWebサービスの企画担当で、独創的で、まだ誰も思いついていないような、新しいサービスや事業やアイデアなどを考えることができます。"},
+        {"role": "user", "content": 
+            f"""
+            アイデアのタイトルと説明を水平思考で考え2つ出してください。
+            # カテゴリ
+            {category}+"に特化したものを考えてください"
+            
+            # アイデア
+            {idea}+"を含ませてください"
+            # 情報
+            ・ターゲット：{target}
+            
+            以下のように出力してください
+            サービスのタイトル(title)そのサービスの詳しい説明やアイデア(explain)をjson形式で出力してください
+            "results":[{{"title":"", "explain":""}}, {{"title":"", "explain":""}}]
+            """}
             ],
+
             response_format={"type": "json_object"}
     )
 
@@ -38,11 +43,14 @@ def index(request):
 
 # 各サービスのタイトルと説明を出力
     for service in data['results']:
-        print(f"タイトル: {service['title']}")
-        print(f"説明: {service['explain']}\n")
+        print(f"{service['title']}")
+        print(f"{service['explain']}\n")
    
     return HttpResponse(completion.choices[0].message.content)
 
 
 
+# 一覧view  
+def top(request):
+    pass
      
