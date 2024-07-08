114a94801dcb0b237baa402dde86846b378bb7bf
yabuki829 <syodai829@icloud.com>
2024-07-08 20:54:21 +0900

    page_sizeの変更

--- api/paginations.py
+++ api/paginations.py
@@ -1,6 +1,6 @@
 from rest_framework.pagination import PageNumberPagination
 
 class IdeaPagination(PageNumberPagination):
-    page_size = 2 
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
